907f07e6fc3b9cbb0d930cb0be6dae8007f3870f
Chovnyuk <kosyakmakc@live.com>
2025-11-02 21:29:28 +0500

    remove specific api on social platform

--- src/main/java/io/github/kosyakmakc/socialBridge/SocialPlatforms/ISocialPlatform.java
+++ src/main/java/io/github/kosyakmakc/socialBridge/SocialPlatforms/ISocialPlatform.java
@@ -1,12 +1,9 @@
 package io.github.kosyakmakc.socialBridge.SocialPlatforms;
 
 import io.github.kosyakmakc.socialBridge.ISocialBridge;
-import io.github.kosyakmakc.socialBridge.MinecraftPlatform.MinecraftUser;
 
 import java.lang.Runtime.Version;
-import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.UUID;
 
 public interface ISocialPlatform {
     void setAuthBridge(ISocialBridge authBridge);
@@ -14,14 +11,7 @@ public interface ISocialPlatform {
     Version getCompabilityVersion();
 
     @SuppressWarnings("unused")
-    void Authorize(SocialUser sender, UUID minecraftId) throws SQLException, AuthorizeDuplicationException;
-
-    @SuppressWarnings("unused")
     void sendMessage(SocialUser telegramUser, String message, HashMap<String, String> placeholders);
 
-    MinecraftUser tryGetMinecraftUser(SocialUser socialUser);
-
-    @SuppressWarnings("unused")
-    boolean logoutUser(SocialUser sender);
     void Start();
 }
